a09e80a3f0953ce07dc5fb7b4e39f4200c7fb433
Liu Chao <liuchao5@xiaomi.com>
2018-04-21 10:30:56 +0800

    添加平半球计算公式

--- app/src/main/java/ball/drive/com/nbaball/ScheduleFragment.kt
+++ app/src/main/java/ball/drive/com/nbaball/ScheduleFragment.kt
@@ -1,25 +1,13 @@
 package ball.drive.com.nbaball
 
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
-import android.widget.LinearLayout
-import android.widget.TextView
-import ball.drive.com.nbaball.entity.TeamGroup
-import ball.drive.com.nbaball.entity.common.CommonProfile
-import ball.drive.com.nbaball.event.TeamItemClickEvent
-import ball.drive.com.nbaball.event.postEvent
-import ball.drive.com.nbaball.network.APIClient
 import org.jetbrains.anko.onClick
-import rx.android.schedulers.AndroidSchedulers
-import rx.schedulers.Schedulers
 import java.text.DecimalFormat
-import java.util.*
 
 /**
  * Created by aaron on 2018/4/11.
@@ -46,6 +34,26 @@ class ScheduleFragment: BaseFragment() {
             view?.findViewById<EditText>(R.id.etRate1).setText(DecimalFormat("0.000").format(et1))
             view?.findViewById<EditText>(R.id.etRate2).setText(DecimalFormat("0.000").format(et2))
             view?.findViewById<EditText>(R.id.etRate3).setText(DecimalFormat("0.000").format(et3))
+        }
+
+        view?.findViewById<Button>(R.id.btReset)?.onClick {
+            view?.findViewById<EditText>(R.id.etR1).setText("")
+            view?.findViewById<EditText>(R.id.etR2).setText("")
+            view?.findViewById<EditText>(R.id.etR3).setText("")
+            view?.findViewById<EditText>(R.id.etRate1).setText("")
+            view?.findViewById<EditText>(R.id.etRate2).setText("")
+            view?.findViewById<EditText>(R.id.etRate3).setText("")
+        }
+
+        view?.findViewById<Button>(R.id.btE1)?.onClick {
+            val r1 = view?.findViewById<EditText>(R.id.etRate1).text.toString().toFloat()
+            val r2 = view?.findViewById<EditText>(R.id.etRate2).text.toString().toFloat()
+            val r3 = view?.findViewById<EditText>(R.id.etRate3).text.toString().toFloat()
+            val x = view?.findViewById<EditText>(R.id.etX1).text.toString().toFloat()
+            val y = view?.findViewById<EditText>(R.id.etY1).text.toString().toFloat()
+
+            view?.findViewById<EditText>(R.id.etEl1)?.setText(DecimalFormat("0.000").format(r1 * x))
+            view?.findViewById<EditText>(R.id.etEr1)?.setText(DecimalFormat("0.000").format(r2* 0.5 * y + r3 * y))
         }
     }
 }
