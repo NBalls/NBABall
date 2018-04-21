7b7f1d31844da85acb799f31ff845705ee762685
Liu Chao <liuchao5@xiaomi.com>
2018-04-21 11:25:35 +0800

    添加半一球计算公式

--- app/src/main/java/ball/drive/com/nbaball/ScheduleFragment.kt
+++ app/src/main/java/ball/drive/com/nbaball/ScheduleFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import org.jetbrains.anko.onClick
 import java.text.DecimalFormat
 
@@ -22,65 +23,76 @@ class ScheduleFragment: BaseFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         view?.findViewById<Button>(R.id.btSubmit)?.onClick {
-            val etNormalRate = view?.findViewById<EditText>(R.id.etNormalRate).text.toString().toFloat()
-            val etR1 = view?.findViewById<EditText>(R.id.etR1).text.toString().toFloat()
-            val etR2 = view?.findViewById<EditText>(R.id.etR2).text.toString().toFloat()
-            val etR3 = view?.findViewById<EditText>(R.id.etR3).text.toString().toFloat()
+            val etNormalRate = view.findViewById<EditText>(R.id.etNormalRate).text.toString().toFloat()
+            val etR1 = view.findViewById<EditText>(R.id.etR1).text.toString().toFloat()
+            val etR2 = view.findViewById<EditText>(R.id.etR2).text.toString().toFloat()
+            val etR3 = view.findViewById<EditText>(R.id.etR3).text.toString().toFloat()
 
             val et1 = etNormalRate / etR1
             val et2 = etNormalRate / etR2
             val et3 = etNormalRate / etR3
 
-            view?.findViewById<EditText>(R.id.etRate1).setText(DecimalFormat("0.000").format(et1))
-            view?.findViewById<EditText>(R.id.etRate2).setText(DecimalFormat("0.000").format(et2))
-            view?.findViewById<EditText>(R.id.etRate3).setText(DecimalFormat("0.000").format(et3))
+            view.findViewById<TextView>(R.id.etRate1).setText(DecimalFormat("0.000").format(et1))
+            view.findViewById<TextView>(R.id.etRate2).setText(DecimalFormat("0.000").format(et2))
+            view.findViewById<TextView>(R.id.etRate3).setText(DecimalFormat("0.000").format(et3))
         }
 
         view?.findViewById<Button>(R.id.btReset)?.onClick {
-            view?.findViewById<EditText>(R.id.etR1).setText("")
-            view?.findViewById<EditText>(R.id.etR2).setText("")
-            view?.findViewById<EditText>(R.id.etR3).setText("")
-            view?.findViewById<EditText>(R.id.etRate1).setText("")
-            view?.findViewById<EditText>(R.id.etRate2).setText("")
-            view?.findViewById<EditText>(R.id.etRate3).setText("")
+            view.findViewById<EditText>(R.id.etR1).setText("")
+            view.findViewById<EditText>(R.id.etR2).setText("")
+            view.findViewById<EditText>(R.id.etR3).setText("")
+            view.findViewById<TextView>(R.id.etRate1).setText("")
+            view.findViewById<TextView>(R.id.etRate2).setText("")
+            view.findViewById<TextView>(R.id.etRate3).setText("")
         }
 
         view?.findViewById<Button>(R.id.btResetTs)?.onClick {
-            view?.findViewById<EditText>(R.id.etX).setText("")
-            view?.findViewById<EditText>(R.id.etY).setText("")
+            view.findViewById<EditText>(R.id.etX).setText("")
+            view.findViewById<EditText>(R.id.etY).setText("")
         }
 
         view?.findViewById<Button>(R.id.btE1)?.onClick {
-            val r1 = view?.findViewById<EditText>(R.id.etRate1).text.toString().toFloat()
-            val r2 = view?.findViewById<EditText>(R.id.etRate2).text.toString().toFloat()
-            val r3 = view?.findViewById<EditText>(R.id.etRate3).text.toString().toFloat()
-            val x = view?.findViewById<EditText>(R.id.etX).text.toString().toFloat()
-            val y = view?.findViewById<EditText>(R.id.etY).text.toString().toFloat()
+            val r1 = view.findViewById<TextView>(R.id.etRate1).text.toString().toFloat()
+            val r2 = view.findViewById<TextView>(R.id.etRate2).text.toString().toFloat()
+            val r3 = view.findViewById<TextView>(R.id.etRate3).text.toString().toFloat()
+            val x = view.findViewById<EditText>(R.id.etX).text.toString().toFloat()
+            val y = view.findViewById<EditText>(R.id.etY).text.toString().toFloat()
 
-            view?.findViewById<EditText>(R.id.etEl1)?.setText(DecimalFormat("0.000").format(r1 * x))
-            view?.findViewById<EditText>(R.id.etEr1)?.setText(DecimalFormat("0.000").format(r2* 0.5 * y + r3 * y))
+            view.findViewById<TextView>(R.id.etEl1)?.text = DecimalFormat("0.000").format(r1 * x)
+            view.findViewById<TextView>(R.id.etEr1)?.text = DecimalFormat("0.000").format(r2* 0.5 * y + r3 * y)
         }
 
         view?.findViewById<Button>(R.id.btE2)?.onClick {
-            val r1 = view?.findViewById<EditText>(R.id.etRate1).text.toString().toFloat()
-            val r2 = view?.findViewById<EditText>(R.id.etRate2).text.toString().toFloat()
-            val r3 = view?.findViewById<EditText>(R.id.etRate3).text.toString().toFloat()
-            val x = view?.findViewById<EditText>(R.id.etX).text.toString().toFloat()
-            val y = view?.findViewById<EditText>(R.id.etY).text.toString().toFloat()
+            val r1 = view.findViewById<TextView>(R.id.etRate1).text.toString().toFloat()
+            val r2 = view.findViewById<TextView>(R.id.etRate2).text.toString().toFloat()
+            val r3 = view.findViewById<TextView>(R.id.etRate3).text.toString().toFloat()
+            val x = view.findViewById<EditText>(R.id.etX).text.toString().toFloat()
+            val y = view.findViewById<EditText>(R.id.etY).text.toString().toFloat()
 
-            view?.findViewById<EditText>(R.id.etEl2)?.setText(DecimalFormat("0.000").format(r1 * x))
-            view?.findViewById<EditText>(R.id.etEr2)?.setText(DecimalFormat("0.000").format(r3 * y))
+            view.findViewById<TextView>(R.id.etEl2)?.text = DecimalFormat("0.000").format(r1 * x)
+            view.findViewById<TextView>(R.id.etEr2)?.text = DecimalFormat("0.000").format(r3 * y)
         }
 
         view?.findViewById<Button>(R.id.btE3)?.onClick {
-            val r1 = view?.findViewById<EditText>(R.id.etRate1).text.toString().toFloat()
-            val r2 = view?.findViewById<EditText>(R.id.etRate2).text.toString().toFloat()
-            val r3 = view?.findViewById<EditText>(R.id.etRate3).text.toString().toFloat()
-            val x = view?.findViewById<EditText>(R.id.etX).text.toString().toFloat()
-            val y = view?.findViewById<EditText>(R.id.etY).text.toString().toFloat()
+            val r1 = view.findViewById<TextView>(R.id.etRate1).text.toString().toFloat()
+            val r2 = view.findViewById<TextView>(R.id.etRate2).text.toString().toFloat()
+            val r3 = view.findViewById<TextView>(R.id.etRate3).text.toString().toFloat()
+            val x = view.findViewById<EditText>(R.id.etX).text.toString().toFloat()
+            val y = view.findViewById<EditText>(R.id.etY).text.toString().toFloat()
 
-            view?.findViewById<EditText>(R.id.etEl3)?.setText(DecimalFormat("0.000").format(r1 * x))
-            view?.findViewById<EditText>(R.id.etEr3)?.setText(DecimalFormat("0.000").format(r2 * y + r3 * y))
+            view.findViewById<TextView>(R.id.etEl3)?.text = DecimalFormat("0.000").format(r1 * x)
+            view.findViewById<TextView>(R.id.etEr3)?.text = DecimalFormat("0.000").format(r2 * y + r3 * y)
+        }
+
+        view?.findViewById<Button>(R.id.btE4)?.onClick {
+            val r1 = view.findViewById<TextView>(R.id.etRate1).text.toString().toFloat()
+            val r2 = view.findViewById<TextView>(R.id.etRate2).text.toString().toFloat()
+            val r3 = view.findViewById<TextView>(R.id.etRate3).text.toString().toFloat()
+            val x = view.findViewById<EditText>(R.id.etX).text.toString().toFloat()
+            val y = view.findViewById<EditText>(R.id.etY).text.toString().toFloat()
+
+            view.findViewById<TextView>(R.id.etEl4)?.text = DecimalFormat("0.000").format(r1 * x - (r2 / (r1 + r2)) * 0.5 * x)
+            view.findViewById<TextView>(R.id.etEr4)?.text = DecimalFormat("0.000").format(r2 * y + r3 * y)
         }
     }
 }
